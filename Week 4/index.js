8d2b4cd91275c10de3011153593b267cf7522d8d
ihtesham jahangir <37655@students.riphah.edu.pk>
2023-08-01 10:02:13 -0700

    Assignment 1

--- Week 4/index.js
+++ Week 4/index.js
@@ -1,1 +1,5 @@
-console.log("hello World");
+var input = require('readline-sync');
+var a = input.question("Enter Number");
+var b = input.question("Enter Number");
+var res = a + b;
+console.log("Result = ".concat(res));
--- Week 4/index.ts
+++ Week 4/index.ts
@@ -1,1 +1,6 @@
-console.log("hello World");
+var input = require('readline-sync');
+
+var a = input.question("Enter Number")
+var b = input.question("Enter Number")
+const res = a + b
+console.log(`Result = ${res}`)
